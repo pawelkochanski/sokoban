bd2bb79f83c6949ae3fc1f956cc4c13f20f4dc30
Paweł Kochański <pawkoch98@gmail..com>
2019-10-15 23:13:37 +0200

    begginings of movement, nothin to see

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ document.querySelectorAll('.menu_button').forEach((button) => {
 const map = [
         "WWWWWWWW",
         "WFFFFFFW", 
+        "WFCFTFFW",
         "WFFFFFFW",
-        "WFFFFFFW",
-        "WFFFPFFW",
-        "WFFFFFFW",
-        "WFFFFFFW",
+        "WTFFPFFW",
+        "WFFCFFTW",
+        "WFFFFCFW",
         "WWWWWWWW",];
+
 
 class Game{
     constructor(difficulty){
@@ -22,8 +23,11 @@ class Game{
             default:
                 this.sizex=8;
                 this.sizey=8;
+                this.crates=3
+                
         }
         debugger;
+        //read map global variable
         const board = document.querySelector('.board');
         for (let indey = 0; indey < this.sizey; indey++) {
             const row = document.createElement('div'); 
@@ -34,17 +38,58 @@ class Game{
                 const img = document.createElement('img');
                 switch(map[indey].charAt(index)){
                     case "W":
-                        img.className = "wall";
+                        img.className = "wall";  
                         break;
+                    case "C":
+                        img.className = "crate";  
+                        break;
+                    case "T":
+                        img.className = "target";  
+                        break;   
                     case "P":
+                        this.posx=index;
+                        this.posy=indey;
                         img.className = "player";
+                        this.player = tile;
                     break;  
                 }
                 tile.append(img);
                 row.append(tile);
             }
             board.prepend(row);    
+            board.addEventListener('keypress', (key) => {
+                move(key.code);
+            })
         }
+    }
+
+    move(key,board){
+        switch(key){
+            case "ArrowUp":
+                this.posy-=1;
+                break;
+            case "ArrowDown":
+                this.posy+=1;
+                break;
+            case "ArrowLeft":
+                this.posx-=1;
+                break;
+            case "ArrowRight":
+                this.posy+=1;
+                break;
+        }
+        for (const key in board.elements) {
+            if (object.hasOwnProperty(key) && key == posy) {
+                const element = object[key];
+            }
+            for (const iter in board.elements) {
+                if (object.hasOwnProperty(iter) && iter == posx) {
+                    const tile = object[iter];
+                }
+                
+            }
+        }
+
     }
 
 }
