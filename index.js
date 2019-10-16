c3a20b79d2929b843438a2e68fdcf0e8cc397f96
Paweł Kochański <pawkoch98@gmail..com>
2019-10-16 14:33:46 +0200

    All is done, enjoy !

--- index.js
+++ index.js
@@ -5,96 +5,229 @@ for (const button of buttons) {
         const game = new Game(button.innerText);
         document.addEventListener('keydown', (key) => {
             //debugger;
-            game.move(key.key);
+            game.player.move(key.key, game);
         });
     })
 }
 
 
-const map = [
+const easy = [
         "WWWWWWWW",
-        "WFFFFFFW", 
-        "WFCFTFFW",
-        "WFFFFFFW",
-        "WTFFPFFW",
-        "WFFCFFTW",
-        "WFFFFCFW",
-        "WWWWWWWW",];
+        "WWWWFFFW", 
+        "WWFCFFFW",
+        "WWFWFFTW",
+        "WFFTWWWW",
+        "WFWCFFWW",
+        "WPCFWFWW",
+        "WFTFFFWW",
+        "WWWWWWWW"
+    ];
 
+const medium = [
+        "WWWWWWWW",
+        "WWWTFWWW",
+        "WWWTFPWW",
+        "WWWFCFWW",
+        "WFFCWWWW",
+        "WFWFWWWW",
+        "WFFFWWWW",
+        "WWWWWWWW"
+    ];
+
+const hard = [
+        "WWWWWWWWW",
+        "WWWWWFFFW",
+        "WWFFFCFFW",
+        "WWFFWTTFW",
+        "WFFCWWWWW",
+        "WTCPWWWWW",
+        "WWWWWWWWw"
+    ];
+
+
+
+class Thing{
+    constructor(posx,posy,tile){
+        this.posx = posx;
+        this.posy = posy;
+        this.tile = tile;
+    }
+    render(){
+        if(this.renderClass!=""){
+            const img = document.createElement('img');
+            img.className = this.renderClass;
+            this.tile.append(img);
+        }
+        
+    }
+
+    clear(){
+        this.tile.innerHTML = "";
+    }
+}
+
+class Wall extends Thing{
+    constructor(posx,posy){
+        super(posx,posy);
+        this.renderClass="wall";
+    }
+}
+
+
+class Movable extends Thing{
+
+    constructor(posx,posy){
+        super(posx,posy);
+    }
+
+    move(key,game){
+        debugger;
+        let newposx = this.posx;
+        let newposy = this.posy;
+        switch(key){
+            case "ArrowUp":
+                newposy-=1;
+                break;
+            case "ArrowDown":
+                newposy+=1;
+                break;
+            case "ArrowLeft":
+                newposx-=1;
+                break;
+            case "ArrowRight":
+                newposx+=1;
+                break;
+        }
+        const destination = game.boardmap[newposy][newposx];
+        if(destination===null){
+            if(this instanceof Crate){
+                debugger;
+                let selector ='div[data-x="' + this.posx +'"][data-y="' +this.posy+ '"].target';
+                let target = document.querySelector(selector);
+                if(target != null){
+                    game.crates++;
+                }
+                selector ='div[data-x="' + newposx +'"][data-y="' +newposy+ '"].target';
+                target = document.querySelector(selector);
+                if(target != null){
+                    game.crates--;
+                    if(game.crates == 0){
+                        game.finish();
+                    }
+                }
+
+            }
+            if(this instanceof Player)
+                game.movesCount++;
+            this.clear();
+            game.boardmap[newposy][newposx] = this;
+            const selector ='div[data-x="' + newposx +'"][data-y="' +newposy+ '"]';
+            this.tile = document.querySelector(selector);
+            game.boardmap[this.posy][this.posx] = null;
+            this.posx=newposx;
+            this.posy=newposy;
+            this.render();
+            return true;
+        }
+        else if(destination instanceof Crate && !(this instanceof Crate))   {
+            if(destination.move(key,game)){
+                this.move(key,game);
+                return true;
+            }
+        }
+        return false;
+            
+    }
+}
+
+class Player extends Movable{
+    constructor(posx,posy){
+        super(posx,posy);
+        this.renderClass="player";
+    }
+
+    
+}
+
+class Crate extends Movable{
+    constructor(posx,posy){
+        super(posx,posy);
+        this.renderClass="crate";
+    }
+
+    
+}
 
 class Game{
 
-    move(key){
-        //debugger;
-        switch(key){
-            case "ArrowUp":
-                this.posy-=1;
-                break;
-            case "ArrowDown":
-                this.posy+=1;
-                break;
-            case "ArrowLeft":
-                this.posx-=1;
-                break;
-            case "ArrowRight":
-                this.posx+=1;
-                break;
-        }
-        const selector ='div[data-x="' + this.posx +'"][data-y="' +this.posy+ '"]';
-        const destination = document.querySelector(selector);
-        this.player.querySelector('img').classList.remove("player");
-        destination.querySelector('img').classList.add("player");
-        this.player = destination;
-        setTimeout(function(){}, 2000);
-
-    }
-
     constructor(difficulty){
+        let map;
         switch(difficulty){
-            default:
-                this.sizex=8;
-                this.sizey=8;
-                this.crates=3
-                this.keyhelper=0;
-                
+            case "Easy":
+                map = easy;
+                break;
+            case "Medium":
+                map = medium;
+                break;
+            case "Hard":
+                map = hard;
+                break;
+            default:     
         }
-        //debugger;
+        this.sizex=map[0].length;
+        this.sizey=map.length;
+        this.crates=0;
+        this.movesCount = 0;
         //read map global variable
         const board = document.querySelector('.board');
+        this.boardmap = [];
         for (let indey = 0; indey < this.sizey; indey++) {
+            //debugger;
             const row = document.createElement('div'); 
             row.classList.add("row");
+            board.append(row);
+            const rowmap = []
             for (let index = 0; index < this.sizex; index++) {
                 const tile = document.createElement('div');
                 tile.setAttribute('data-x',index.toString());
                 tile.setAttribute('data-y',indey.toString());
                 tile.classList.add("floor");
-                const img = document.createElement('img');
+                let thing = null;
                 switch(map[indey].charAt(index)){
                     case "W":
-                        img.className = "wall";  
+                        thing = new Wall(index,indey); 
                         break;
                     case "C":
-                        img.className = "crate";  
+                        thing = new Crate(index,indey);
+                        this.crates++;
                         break;
                     case "T":
                         tile.classList.remove("floor"); 
                         tile.classList.add("target");
                         break;   
                     case "P":
-                        this.posx=index;
-                        this.posy=indey;
-                        img.className = "player";
-                        this.player = tile;
+                        thing = new Player(index,indey);
+                        this.player = thing;
                     break;  
                 }
-                tile.append(img);
+                rowmap[index] = thing;
                 row.append(tile);
+                if(thing!=null){
+                    thing.tile = tile;
+                    thing.render();
+                }
+                
             }
-            board.append(row);
+            
+            this.boardmap[indey] = rowmap;
         }
     }
 
-    
+    finish(){
+        debugger;
+        const board = document.querySelector('.board');
+        board.innerHTML="<h1>Congratulations! You Won :D</h1><h2>You beat this level with " + this.movesCount + " moves!</h2><h2>If you wanna go again just refresh the page!</h2>";
+    }
 
+    
 }
