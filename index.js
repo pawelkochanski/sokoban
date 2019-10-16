cd6856f3dd4333156ab449d56522b0ebcbd99d6d
Paweł Kochański <pawkoch98@gmail..com>
2019-10-16 12:13:27 +0200

    Some board fixing stuff

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-document.querySelectorAll('.menu_button').forEach((button) => {
-    console.log(button);
+const buttons = document.querySelectorAll('.menu_button');
+for (const button of buttons) {
     button.addEventListener('click', () =>{
         document.querySelector('.menu').classList.add('hidden');
         const game = new Game(button.innerText);
+        document.addEventListener('keydown', (key) => {
+            //debugger;
+            game.move(key.key);
+        });
     })
-});
+}
+
 
 const map = [
         "WWWWWWWW",
@@ -18,52 +23,9 @@ const map = [
 
 
 class Game{
-    constructor(difficulty){
-        switch(difficulty){
-            default:
-                this.sizex=8;
-                this.sizey=8;
-                this.crates=3
-                
-        }
-        debugger;
-        //read map global variable
-        const board = document.querySelector('.board');
-        for (let indey = 0; indey < this.sizey; indey++) {
-            const row = document.createElement('div'); 
-            row.classList.add("row");
-            for (let index = 0; index < this.sizex; index++) {
-                const tile = document.createElement('div');
-                tile.classList.add("tile");
-                const img = document.createElement('img');
-                switch(map[indey].charAt(index)){
-                    case "W":
-                        img.className = "wall";  
-                        break;
-                    case "C":
-                        img.className = "crate";  
-                        break;
-                    case "T":
-                        img.className = "target";  
-                        break;   
-                    case "P":
-                        this.posx=index;
-                        this.posy=indey;
-                        img.className = "player";
-                        this.player = tile;
-                    break;  
-                }
-                tile.append(img);
-                row.append(tile);
-            }
-            board.prepend(row);    
-            board.addEventListener('keypress', (key) => {
-                move(key.code);
-            })
-        }
-    }
 
-    move(key,board){
+    move(key){
+        //debugger;
         switch(key){
             case "ArrowUp":
                 this.posy-=1;
@@ -75,21 +37,64 @@ class Game{
                 this.posx-=1;
                 break;
             case "ArrowRight":
-                this.posy+=1;
+                this.posx+=1;
                 break;
         }
-        for (const key in board.elements) {
-            if (object.hasOwnProperty(key) && key == posy) {
-                const element = object[key];
-            }
-            for (const iter in board.elements) {
-                if (object.hasOwnProperty(iter) && iter == posx) {
-                    const tile = object[iter];
-                }
-                
-            }
-        }
+        const selector ='div[data-x="' + this.posx +'"][data-y="' +this.posy+ '"]';
+        const destination = document.querySelector(selector);
+        this.player.querySelector('img').classList.remove("player");
+        destination.querySelector('img').classList.add("player");
+        this.player = destination;
+        setTimeout(function(){}, 2000);
 
     }
+
+    constructor(difficulty){
+        switch(difficulty){
+            default:
+                this.sizex=8;
+                this.sizey=8;
+                this.crates=3
+                this.keyhelper=0;
+                
+        }
+        //debugger;
+        //read map global variable
+        const board = document.querySelector('.board');
+        for (let indey = 0; indey < this.sizey; indey++) {
+            const row = document.createElement('div'); 
+            row.classList.add("row");
+            for (let index = 0; index < this.sizex; index++) {
+                const tile = document.createElement('div');
+                tile.setAttribute('data-x',index.toString());
+                tile.setAttribute('data-y',indey.toString());
+                tile.classList.add("floor");
+                const img = document.createElement('img');
+                switch(map[indey].charAt(index)){
+                    case "W":
+                        img.className = "wall";  
+                        break;
+                    case "C":
+                        img.className = "crate";  
+                        break;
+                    case "T":
+                        tile.classList.remove("floor"); 
+                        tile.classList.add("target");
+                        break;   
+                    case "P":
+                        this.posx=index;
+                        this.posy=indey;
+                        img.className = "player";
+                        this.player = tile;
+                    break;  
+                }
+                tile.append(img);
+                row.append(tile);
+            }
+            board.append(row);
+        }
+    }
+
+    
 
 }
